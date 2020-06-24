aaca85f12622ed598f6c8e33c9da193b124d7545
Sarah Vivoli <snvivoli@gmail.com>
2020-06-24 10:32:38 -0700

    logo component

---
+++ client/src/components/Logo/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Logo() {
+    return(
+        <div>
+            <img src={require('../../img/logo_grey_thick.png')}></img>
+        </div>
+    )
+};
+
+export default Logo;
---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo from "../Logo";
+
+function Navbar() {
+    return(
+        <Logo />
+    )
+};
+
+export default Navbar;
