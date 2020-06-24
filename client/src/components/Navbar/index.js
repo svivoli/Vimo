e9faa51e3031082357cb6c9bc8b4d3fbdf1c5671
Sarah Vivoli <snvivoli@gmail.com>
2020-06-24 11:16:23 -0700

    navbar style

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,26 +3,13 @@ import './style.css';
 
 function Navbar() {
     return (
-        <nav className="navbar uk-navbar-container" uk-navbar>
-            <div className="uk-navbar-center">
-
-                <ul className="uk-navbar-nav">
-                    <li className="uk-active"><a href="#">Active</a></li>
-                    <li>
-                        <a href="#">Parent</a>
-                        <div className="uk-navbar-dropdown">
-                            <ul className="uk-nav uk-navbar-dropdown-nav">
-                                <li className="uk-active"><a href="#">Active</a></li>
-                                <li><a href="#">Item</a></li>
-                                <li><a href="#">Item</a></li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li><a href="#">Item</a></li>
-                </ul>
-
-            </div>
-        </nav>
+        <div className="navbar">
+            <ul>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Credentials</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
     )
 };
 
