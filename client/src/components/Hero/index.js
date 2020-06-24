2858204a1f67a018f7c842c23db1515de03e0c9e
Sarah Vivoli <snvivoli@gmail.com>
2020-06-24 12:53:05 -0700

    hero text

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import './style.css';
 
 function Hero() {
-    return(
+    return (
         <section className="hero">
-            <img src={require('../../img/hero.png')}></img>
+            <div className="row">
+                <div className="col-md-8">
+                    <img className="hero-img" src={require('../../img/hero.png')}></img>
+                </div>
+                <div className="col-md-4">
+                    <div className="hero-text">
+                        <h2>Branded With Care</h2>
+                        <h3>Website Design, Creation, Maintenance</h3>
+
+                        <h4>Component based site structure allows for continued flexibility in design and style.</h4>
+
+                        <h3>Inquire for consultation.</h3>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 };
