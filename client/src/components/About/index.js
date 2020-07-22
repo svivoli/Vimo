a2f6098f9f82af410d064a7f3ee8c07067944915
Sarah Vivoli <snvivoli@gmail.com>
2020-07-22 11:40:55 -0700

    about section photos

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -10,12 +10,10 @@ function About() {
             </div>
             <div>
                 <h3>Who We Are</h3>
-                <div className="row">
-                    <div className="col-md-6">
-                        <img className="headshot" src={require('../../img/sarah.png')}></img>
-                    </div>
-                    <div className="col-md-6">
-                        <img className="headshot" src={require('../../img/sarah.png')}></img>
+                <div className="row justify-content-center">
+                    <div className="headshots col-md-4">
+                        <img className="headshot" src={require('../../img/jenni.png')}></img>
+                        <img className="headshot" src={require('../../img/sarah1.png')}></img>
                     </div>
                 </div>
                 <p>Jenni and Sarah are two Portland based web developers who recently graduated from the University of Oregon Coding Boot Camp. We specialize in creating design forward websites with streamlined user interfaces. We understand that a website is a major part of a company's brand, and should curate a unique experience for the user while embodying the identity of the individual or company. The main technologies we use are HTML, CSS, JavaScript, Node, Express, and React. Refer to the section below to view the other technologies we use.</p>
